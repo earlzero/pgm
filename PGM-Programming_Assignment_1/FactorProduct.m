2acdf2c333939784cd5c6107824740c43df6fc0a
Nikita Sapozhnikov <earlzero@yandex.ru>
2013-06-09 17:22:05 +0400

    FactorProduct & FactorMarginalization

--- PGM-Programming_Assignment_1/FactorProduct.m
+++ PGM-Programming_Assignment_1/FactorProduct.m
@@ -60,8 +60,9 @@ indxB = AssignmentToIndex(assignments(:, mapB), B.card);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE:
 % Correctly populate the factor values of C
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+for i=1:size(C.val,2)
+	C.val(i)=A.val(indxA(i))*B.val(indxB(i));
+end
 end
